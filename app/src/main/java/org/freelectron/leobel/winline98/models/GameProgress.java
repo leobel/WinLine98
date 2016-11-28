72b5038e3670b8374d1077bd21a83928cc9fd91d
Leobel Izquierdo <leobelizquierdo@gmail.com>
2016-11-28 12:25:35 -0500

    add GameProgress model

---
+++ app/src/main/java/org/freelectron/leobel/winline98/models/GameProgress.java
@@ -0,0 +1,18 @@
+package org.freelectron.leobel.winline98.models;
+
+/**
+ * Created by leobel on 11/28/16.
+ */
+
+public class GameProgress {
+
+    private int score;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
